9646dc8e716b1521760a4711f1a6d87d32e7f08c
polosa00 <i@apolochanin.ru>
2023-06-16 13:40:22 +0300

    create model

---
+++ StateAnaDataFlow/User.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  StateAnaDataFlow
+//
+//  Created by Александр Полочанин on 16.06.23.
+//
+
+import Foundation
+
+
+struct User {
+    var name: String
+    var isLogged: Bool
+}
